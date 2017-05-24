f616b6c3a9339924c85880a85ff2a849a5d87db6
timur <tim8861@yandex.com>
2017-05-24 10:44:37 +0300

    Initial commit

---
+++ top_page_words.py
@@ -0,0 +1,81 @@
+import requests
+from bs4 import BeautifulSoup
+import nltk
+import string
+import collections
+import json
+
+def parse_page_text(page):
+    document = BeautifulSoup(page.text, 'html.parser')
+    text = ''
+    for i in document.find_all('p', recursive=True):
+        text += i.get_text() + ' '
+    return text
+
+def get_stemms(page_text):
+    tokens = nltk.word_tokenize(page_text)                                #Tokenize words
+    tokens = [i for i in tokens if (i not in string.punctuation)]         #Clean punctuation
+
+    stop_words = nltk.corpus.stopwords.words('english')                   #The list of common words
+    tokens = [i for i in tokens if ( i not in stop_words )]               #Clean common words 'as', 'the', etc
+
+    stemms = [i.lower() for i in stemms_generator(tokens)]
+    return stemms
+
+def stemms_generator(tokens):
+    stemmer = nltk.stem.SnowballStemmer("english")                        #Create stemmer
+    for t in tokens:
+        yield stemmer.stem(t)
+
+def top_20(stemms):
+    stemms = collections.Counter(stemms)
+    stemms = sorted([(v, k) for (k, v) in stemms.items()], reverse=True)  #reverse (k, v) and sort
+
+    elem_counter = 1
+    prev_k = None
+    top_list = []                                                         #All entities
+
+    for st in stemms:
+        (k, v) = st
+        if k != prev_k:
+            tmp_top_list = {'rank': 0, 'count': 0, 'words': []}           #One entity
+            top_list.append(tmp_top_list)
+            tmp_top_list['rank']  = elem_counter
+            tmp_top_list['count'] = k
+            tmp_top_list['words'] = []
+            elem_counter += 1
+            prev_k = k
+        if elem_counter > 21:
+            break
+        tmp_top_list['words'].append(v)
+
+    return top_list
+
+def export_to_json(file_path, stemms):
+    with open(file_path, 'w') as outfile:
+        json.dump(stemms, outfile, indent=4)
+
+
+'''
+The Task
+
+Download web page using HTTP Protocol (requests module)
+Parse loaded data with bs4
+
+Tokenize and tag your data (nltk module)
+Write popular noun (top 20 noun) with count in the new file in json format.
+'''
+
+url = 'https://www.tutorialspoint.com/sambo/sambo_rules.htm'
+page = requests.get(url)
+
+page_text = parse_page_text(page)
+
+stemms = get_stemms(page_text)
+
+popular_stemms = top_20(stemms)
+
+file_path = 'output/top20.json'
+export_to_json(file_path, popular_stemms)
+
+print('Task completed succesfully, you can find json file with top 20 words in a file', file_path)
